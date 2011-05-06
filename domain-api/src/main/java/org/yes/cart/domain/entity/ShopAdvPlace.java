3c447e1a70102039953aeee40586151bc55971c7
azarny <azarny@gmail.com>
2011-05-06 15:06:41 +0000

    [YC-1] Domain api initial revision. 26

---
+++ domain-api/src/main/java/org/yes/cart/domain/entity/ShopAdvPlace.java
@@ -0,0 +1,47 @@
+
+package org.yes.cart.domain.entity;
+
+import java.util.Set;
+
+/**
+ * Named advertising place in the shop .
+ *
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 07-May-2011
+ * Time: 11:12:54
+ */
+public interface ShopAdvPlace extends Auditable {
+
+    /**
+     */
+    public long getShopadvplaceId();
+
+    public void setShopadvplaceId(long shopadvplaceId);
+
+    /**
+     */
+    public String getName();
+
+    public void setName(String name);
+
+    /**
+     */
+    public String getDescription();
+
+    public void setDescription(String description);
+
+    /**
+     */
+    public Shop getShop();
+
+    public void setShop(Shop shop);
+
+    /**
+     */
+    public Set<ShopAdvPlaceRule> getShopAdvPlaceRules();
+
+    public void setShopAdvPlaceRules(Set<ShopAdvPlaceRule> shopAdvPlaceRules);
+
+}
+
+
