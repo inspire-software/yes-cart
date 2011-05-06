c7f9eec9b74d0006bb70d696ad6b9b3f0ff94241
azarny@gmail.com <azarny@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2011-05-06 15:16:55 +0000

    [YC-1] Domain api initial revision. 35

---
+++ trunk/domain-api/src/main/java/org/yes/cart/domain/queryobject/impl/FiteredNavigationRecordImpl.java
@@ -0,0 +1,138 @@
+package org.yes.cart.domain.queryobject.impl;
+
+import org.yes.cart.domain.queryobject.FiteredNavigationRecord;
+
+import java.io.Serializable;
+
+/**
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 07-May-2011
+ * Time: 11:12:54
+ */
+public class FiteredNavigationRecordImpl implements FiteredNavigationRecord, Serializable {
+
+    private String name;
+
+    private String code;
+
+    private String value;
+
+    private int count;
+
+    private int rank;
+
+    private String type;
+
+    /** {@inheritDoc */
+    public String getCode() {
+        return code;
+    }
+
+    /** {@inheritDoc */
+    public void setCode(final String code) {
+        this.code = code;
+    }
+
+    /** {@inheritDoc */
+    public String getValue() {
+        return value;
+    }
+
+    /** {@inheritDoc */
+    public void setValue(final String value) {
+        this.value = value;
+    }
+
+    /** {@inheritDoc */
+    public int getCount() {
+        return count;
+    }
+
+    /** {@inheritDoc */
+    public void setCount(final int itemsCount) {
+        this.count = itemsCount;
+    }
+
+    /** {@inheritDoc */
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    /** {@inheritDoc */
+    public String getName() {
+        return name;
+    }
+
+    /** {@inheritDoc */
+    public int getRank() {
+        return rank;
+    }
+
+    /** {@inheritDoc */
+    public void setRank(final int rank) {
+        this.rank = rank;
+    }
+
+    /** {@inheritDoc */
+    public String getType() {
+        return type;
+    }
+
+    /** {@inheritDoc */
+    public void setType(final String type) {
+        this.type = type;
+    }
+
+    /**
+     * Construct filtered navigation record.
+     * @param name attribute nave
+     * @param code attribute code
+     * @param value value
+     * @param count count of objects.
+     */
+    public FiteredNavigationRecordImpl(final String name, final String code, final String value, final int count) {
+        this.name = name;
+        this.code = code;
+        this.value = value;
+        this.count = count;
+    }
+
+    /**
+     * Construct filtered navigation record.
+     * @param name attribute nave
+     * @param code attribute code
+     * @param value value
+     * @param count count of objects.
+     * @param rank rank
+     */
+    public FiteredNavigationRecordImpl(final String name, final String code, final String value, final int count, final int rank) {
+        this.name = name;
+        this.code = code;
+        this.value = value;
+        this.count = count;
+        this.rank = rank;
+    }
+
+    /**
+     * Construct filtered navigation record.
+     * @param name attribute nave
+     * @param code attribute code
+     * @param value value
+     * @param count count of objects.
+     * @param rank rank
+     * @param type type of navigation S - single value R - range value
+     */
+    public FiteredNavigationRecordImpl(final String name, final String code, final String value,
+                                       final int count, final int rank, final String type) {
+        this.name = name;
+        this.code = code;
+        this.value = value;
+        this.count = count;
+        this.rank = rank;
+        this.type = type;
+    }
+
+
+
+
+}
