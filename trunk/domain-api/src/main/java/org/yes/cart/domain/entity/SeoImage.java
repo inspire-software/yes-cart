7cd9cb66625cd5bab6361144faeeac7c9c948cb0
azarny@gmail.com <azarny@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2011-05-06 15:06:41 +0000

    [YC-1] Domain api initial revision. 26

---
+++ trunk/domain-api/src/main/java/org/yes/cart/domain/entity/SeoImage.java
@@ -0,0 +1,60 @@
+package org.yes.cart.domain.entity;
+
+/**
+ * Search engine optimizations on images.
+ * <p/>
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 07-May-2011
+ * Time: 11:12:54
+ */
+public interface SeoImage extends Auditable {
+
+    /**
+     * Get image name.
+     * @return image name.
+     */
+    String getImageName();
+
+    /**
+     * Set image name.
+     * @param imageName image name.
+     */
+    void setImageName(String imageName);
+
+    /**
+     * Get alternative text.
+     * @return alternative text
+     */
+    String getAlt();
+
+
+    /**
+     * Set alternative text.
+     * @param alt alternative text
+     */
+    void setAlt(String alt);
+
+    /**
+     * Get image title.
+     * @return image title
+     */
+    String getTitle();
+
+    /**
+     * Set image title.
+     * @param title image title.
+     */
+    void setTitle(String title);
+
+    /**
+     * Get pk value.
+     * @return pk value
+     */
+    long getSeoImageId();
+
+    /**
+     * Set pk value.
+     * @param seoImageId pk value.
+     */
+    void setSeoImageId(long seoImageId);
+}
---
+++ trunk/domain-api/src/main/java/org/yes/cart/domain/entity/Shop.java
@@ -0,0 +1,226 @@
+package org.yes.cart.domain.entity;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+
+/**
+ * Shop.
+ *
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 07-May-2011
+ * Time: 11:12:54
+
+ */
+
+public interface Shop extends Auditable {
+
+    /**
+     * Get shop code.
+     * @return shop code.
+     */
+    String getCode();
+
+    /**
+     * Set shop code.
+     * @param code shop code.
+     */
+    void setCode(String code);
+
+
+    /**
+     * Get pk value.
+     * @return pk value.
+     */
+    long getShopId();
+
+    /**
+     * Set pk value.
+     * @param shopId shop pk value.
+     */
+    void setShopId(long shopId);
+
+    /**
+     * Get shop name.
+     * @return shop name.
+     */
+    String getName();
+
+    /**
+     * Set shop name.
+     * @param name shop name.
+     */
+    void setName(String name);
+
+    /**
+     * Get shop description.
+     * @return description.
+     */
+    String getDescription();
+
+    /**
+     * Set shop description.
+     * @param description shop description
+     */
+    void setDescription(String description);
+
+    /**
+     * Get the path to shop theme. Default failover path will be used,
+     * if some resource con not be found in shop theme.
+     * @return path to shop theme.
+     */
+    String getFspointer();
+
+    /**
+     * Set shop theme path.
+     * @param fspointer path to theme.
+     */
+    void setFspointer(String fspointer);
+
+    /**
+     * Get shop supported urls. Example shop.domain, www.shop.domain, www1.shop.domain, wap.shop.domain.
+     * @return list of supported urls.
+     */
+    Set<ShopUrl> getShopUrl();
+
+    /**
+     * Set list of supported urls.
+     * @param shopUrl supported urls.
+     */
+    void setShopUrl(Set<ShopUrl> shopUrl);
+
+    /**
+     * Get the currency exchange rates. All exchange rates from default to target currency.
+     * @return list of exchange rates.
+     */
+    Collection<ShopExchangeRate> getExchangerates();
+
+    /**
+     * Set currency exchange rates.
+     * @param exchangerates  exchange rates.
+     */
+    void setExchangerates(Collection<ShopExchangeRate> exchangerates);
+
+    /**
+     * Get the named advertizing places.
+     * @return named advertizing places.
+     */
+    Collection<ShopAdvPlace> getAdvertisingPlaces();
+
+    /**
+     * Set named advertizing places.
+     * @param advertisingPlaces named advertizing places.
+     */
+    void setAdvertisingPlaces(Collection<ShopAdvPlace> advertisingPlaces);
+
+    /**
+     * Get the discount rules.
+     * @return discount rules.
+     */
+    Collection<ShopDiscount> getShopDiscountRules();
+
+    /**
+     * Set discount rules.
+     * @param shopDiscountRules discount rules.
+     */
+    void setShopDiscountRules(Collection<ShopDiscount> shopDiscountRules);
+
+
+    /**
+     * Get all  attributes.
+     *
+     * @return collection of product attributes.
+     */
+    Collection<AttrValueShop> getAttribute();
+
+    /**
+     * Get all  attributes filtered by given attribute code.
+     *
+     * @param attributeCode code of attribute
+     * @return collection of product attributes filtered by attribute name or empty collection if no attribute were found.
+     */
+    Collection<AttrValueShop> getAttributesByCode(String attributeCode);
+
+    /**
+     * Is B2B profile set.
+     *
+     * @return true is B2B profile is set.
+     */
+    boolean isB2ProfileActive();
+
+
+
+    /**
+     * Get single attribute.
+     *
+     * @param attributeCode code of attribute
+     * @return single {@link AttrValue} or null if not found.
+     */
+    AttrValueShop getAttributeByCode(String attributeCode);
+
+
+    /**
+     * Set collection of  attributes.
+     *
+     * @param attribute collection of attributes
+     */
+    void setAttribute(Collection<AttrValueShop> attribute);
+
+
+    /**
+     * Get seo information about shop.
+     *
+     * @return seo info.
+     */
+    Seo getSeo();
+
+    /**
+     * Set shop seo information.
+     * @param seo seo information.
+     */
+    void setSeo(Seo seo);
+
+    /**
+     * Get categories, that assigned to shop.
+     * @return categories, that assigned to shop.
+     */
+    Collection<ShopCategory> getShopCategory();
+
+    /**
+     * Set categories, that assigned to shop.
+     * @param shopCategory categories, that assigned to shop.
+     */
+    void setShopCategory(Collection<ShopCategory> shopCategory);
+
+    /**
+     * Get the default shop currency.
+     *
+     * @return default currency code or null is there no settings.
+     */
+    String getDefaultCurrency();
+
+    /**
+     * Get all supported currensies. First in list is default.
+     *
+     * @return separated by comma all supported currencies by shop or null if not set.
+     */
+    String getSupportedCurrensies();
+
+    /**
+     * Set supported currency
+     * @param currensies comma separated list of supported currencies.
+     */
+    //void setSupportedCurrensies(String currensies);
+
+    /**
+     * Get all supported currensies. First in list is default.
+     *
+     * @return list of currency codes.
+     */
+    List<String> getSupportedCurrensiesAsList();
+
+
+}
+
+
