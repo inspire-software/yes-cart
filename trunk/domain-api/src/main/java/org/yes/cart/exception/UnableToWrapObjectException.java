57d68ab7fc402f6cc4b3d0f350bac995ebfb8d2f
azarny@gmail.com <azarny@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2011-05-06 15:17:32 +0000

    [YC-1] Domain api initial revision. 36

---
+++ trunk/domain-api/src/main/java/org/yes/cart/exception/UnableToWrapObjectException.java
@@ -0,0 +1,40 @@
+package org.yes.cart.exception;
+
+/**
+ * Exception to be thrown when a domain object cannot be wrapped to DTO.
+ * <p/>
+ * User: dogma
+ * Date: Jan 24, 2011
+ * Time: 1:33:31 PM
+ */
+public class UnableToWrapObjectException extends Exception {
+
+    private static final long serialVersionUID = 20100124L;
+
+    /**
+     * Default constructor.
+     *
+     * @param domainClass domain class to wrap
+     * @param dtoClass dto wrapper
+     * @param cause the cause of failure
+     */
+    public UnableToWrapObjectException(final Class< ? > domainClass,
+                                       final Class< ? > dtoClass,
+                                       final Throwable cause) {
+        super(generateMessage(domainClass, dtoClass, cause));
+
+    }
+
+    private static String generateMessage(final Class< ? > domainClass,
+                                          final Class< ? > dtoClass,
+                                          final Throwable cause) {
+        return new StringBuilder()
+                .append("Unable to wrap ")
+                .append(domainClass.getCanonicalName())
+                .append(" to ")
+                .append(dtoClass.getCanonicalName())
+                .append(" due to: ")
+                .append(cause.getMessage()).toString();
+    }
+
+}
