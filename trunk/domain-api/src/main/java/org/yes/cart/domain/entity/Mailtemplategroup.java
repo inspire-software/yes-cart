a5f153a12bf16a6de649d5ee4590610f984cf0ad
azarny@gmail.com <azarny@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2011-05-06 15:02:10 +0000

    [YC-1] Domain api initial revision. 22

---
+++ trunk/domain-api/src/main/java/org/yes/cart/domain/entity/Mailtemplategroup.java
@@ -0,0 +1,34 @@
+
+package org.yes.cart.domain.entity;
+
+
+/**
+ * Mail template group.
+ *
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 07-May-2011
+ * Time: 11:12:54
+ */
+public interface Mailtemplategroup extends Auditable {
+
+    /**
+     */
+    public long getMailtemplategroupId();
+
+    public void setMailtemplategroupId(long mailtemplategroupId);
+
+    /**
+     */
+    public String getName();
+
+    public void setName(String name);
+
+    /**
+     */
+    public String getDescription();
+
+    public void setDescription(String description);
+
+}
+
+
