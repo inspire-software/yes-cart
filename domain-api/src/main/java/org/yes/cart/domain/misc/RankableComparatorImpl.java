e6e759ec827d8fb0ea1b588d9c1e3d3f9c8524e3
azarny <azarny@gmail.com>
2011-05-06 15:16:12 +0000

    [YC-1] Domain api initial revision. 34

---
+++ domain-api/src/main/java/org/yes/cart/domain/misc/RankableComparatorImpl.java
@@ -0,0 +1,20 @@
+package org.yes.cart.domain.misc;
+
+import org.yes.cart.domain.entity.Rankable;
+
+import java.util.Comparator;
+
+/**
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 07-May-2011
+ * Time: 11:12:54
+
+ */
+public class RankableComparatorImpl implements Comparator<Rankable> {
+
+    /** {@inheritDoc} */
+	public int compare(final Rankable object1, final Rankable object2) {
+		return object1.getRank() - object2.getRank();
+	}
+
+}
---
+++ domain-api/src/main/java/org/yes/cart/domain/misc/SkuPriceSkuNameComparatorImpl.java
@@ -0,0 +1,16 @@
+package org.yes.cart.domain.misc;
+
+import org.yes.cart.domain.entity.SkuPrice;
+
+import java.util.Comparator;
+
+/**
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 07-May-2011
+ * Time: 11:13:01
+ */
+public class SkuPriceSkuNameComparatorImpl  implements Comparator<SkuPrice> {
+    public int compare(SkuPrice skuPrice1, SkuPrice skuPrice2) {
+        return skuPrice1.getSku().getName().compareTo(skuPrice2.getSku().getName());
+    }
+}
