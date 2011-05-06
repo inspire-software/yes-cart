1290522c647be3b63099a44e2d8909aafb0ff3a9
azarny <azarny@gmail.com>
2011-05-06 14:52:31 +0000

    [YC-1] Domain api initial revision. 13 set

---
+++ domain-api/src/main/java/org/yes/cart/domain/entity/Association.java
@@ -0,0 +1,69 @@
+
+package org.yes.cart.domain.entity;
+
+
+/**
+ * Associations between products. It can be:
+ * Cross sell - other related products from other categories.
+ * up sell - the simular products with higher prices.
+ * accesories - accessories to product, like charger for mobile phone.
+ * who buy also buy - dynamic association, base on different customers buy history.
+ *
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 07-May-2011
+ * Time: 11:12:54
+ */
+public interface Association extends Auditable {
+
+
+    String CROSS_SELL = "cross";
+    String ACCESSORIES = "accessories";
+    String UP_SELL = "up";
+    String BUY_WITH_THIS = "buywiththis";
+
+    /**
+     * @return pkimary key.
+     */
+    public long getAssociationId();
+
+    /**
+     * @param associationId primary key to set
+     */
+    public void setAssociationId(long associationId);
+
+    /**
+     * Unique human readable association code.
+     *
+     * @return unique human readable association code
+     */
+    public String getCode();
+
+    /**
+     * @param code unique human readable association code to use.
+     */
+    public void setCode(String code);
+
+    /**
+     * @return Association name.
+     */
+    public String getName();
+
+    /**
+     * @param name association name.
+     */
+    public void setName(String name);
+
+    /**
+     * @return description
+     */
+    public String getDescription();
+
+    /**
+     * @param description description to use.
+     */
+    public void setDescription(String description);
+
+
+}
+
+
---
+++ domain-api/src/main/java/org/yes/cart/domain/entity/Attributable.java
@@ -0,0 +1,68 @@
+package org.yes.cart.domain.entity;
+
+import java.util.Collection;
+
+/**
+ *
+ * Mark objects as attributable.
+ *
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 07-May-2011
+ * Time: 11:12:54
+ */
+public interface Attributable {
+
+    /**
+     * Get all attibute values.
+     * @return  all attr values.
+     */
+    Collection<AttrValue> getAllAttibutes();
+
+
+    /**
+     * Get all  attributes filtered by given attribute code.
+     *
+     * @param attributeCode code of attribute
+     * @return collection of  attributes filtered by attribute name or empty collection if no attribute were found.
+     */
+    Collection/*<AttrValue>*/ getAttributesByCode(String attributeCode);
+
+    /**
+     * Get single attribute.
+     *
+     * @param attributeCode code of attribute
+     * @return single {@link AttrValue} or null if not found.
+     */
+    AttrValue getAttributeByCode(String attributeCode);
+
+
+    /**
+     * Get category name.
+     *
+     * @return category name.
+     */
+    String getName();
+
+    /**
+     * Set category name.
+     *
+     * @param name category name.
+     */
+    void setName(String name);
+
+    /**
+     * Get category description.
+     *
+     * @return category decription.
+     */
+    String getDescription();
+
+    /**
+     * Set description
+     *
+     * @param description description
+     */
+    void setDescription(String description);
+
+
+}
