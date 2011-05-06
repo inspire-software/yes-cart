19440ce108029bef15a77937737a5209a80519a4
azarny <azarny@gmail.com>
2011-05-06 15:18:09 +0000

    [YC-1] Domain api initial revision. 37

---
+++ domain-api/src/main/java/org/yes/cart/service/domain/AttributeManageGenericService.java
@@ -0,0 +1,21 @@
+package org.yes.cart.service.domain;
+
+/**
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 07-May-2011
+ * Time: 11:13:01
+
+ */
+public interface AttributeManageGenericService<T> extends GenericService <T> {
+
+    /**
+     * Set attribute value. New attribute value will be created,
+     * if attribute has not value for given shop.
+     * @param entityId entity pk value
+     * @param attributeKey attribute key
+     * @param attributeValue attribute value.
+     */
+    void updateAttributeValue(long entityId, String attributeKey, String attributeValue);
+
+
+}
