a5a6da719e2dfc466c4aafe6aa9d5adc56cf72d4
azarny <azarny@gmail.com>
2011-05-06 14:43:22 +0000

    [YC-1] Domain api initial revision. 8 set

---
+++ domain-api/src/main/java/org/yes/cart/domain/dto/CartItem.java
@@ -0,0 +1,32 @@
+package org.yes.cart.domain.dto;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * Cart item object that hold information about a single shopping item.
+ *
+ * User: dogma
+ * Date: Jan 15, 2011
+ * Time: 10:25:01 PM
+ */
+public interface CartItem extends Serializable {
+
+    /**
+     *
+     * @return product sku code.
+     */
+    String getProductSkuCode();
+
+    /**
+     * @return quantity of the above sku to be purchased
+     */
+    BigDecimal getQty();
+
+    /**
+     * Get the sku price price.
+     * @return sku price.
+     */
+    BigDecimal getPrice();
+
+}
