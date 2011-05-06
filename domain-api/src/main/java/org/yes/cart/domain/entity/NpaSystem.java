53e5eb3b3996ae704ebee80d7f513b6138ad6721
azarny <azarny@gmail.com>
2011-05-06 15:03:07 +0000

    [YC-1] Domain api initial revision. 23

---
+++ domain-api/src/main/java/org/yes/cart/domain/entity/NpaSystem.java
@@ -0,0 +1,30 @@
+package org.yes.cart.domain.entity;
+
+import java.util.Map;
+
+/**
+ *
+ * TODO kill this interface.
+ */
+public interface NpaSystem extends Auditable {
+    
+    String getCode();
+
+    void setCode(String code);
+
+    String getName();
+
+    void setName(String name);
+
+    String getDescription();
+
+    void setDescription(String description);
+
+    public Map<String, AttrValue> getAttribute();
+
+    public void setAttribute(Map<String, AttrValue> attribute);
+
+    long getNpaSystemId();
+
+    void setNpaSystemId(long npaSystemId);
+}
