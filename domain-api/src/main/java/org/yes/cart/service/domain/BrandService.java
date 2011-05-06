9b412e8ffa1f52b2a39720379a2723e85b5cc8bf
azarny <azarny@gmail.com>
2011-05-06 15:18:53 +0000

    [YC-1] Domain api initial revision. 38

---
+++ domain-api/src/main/java/org/yes/cart/service/domain/BrandService.java
@@ -0,0 +1,12 @@
+package org.yes.cart.service.domain;
+
+import org.yes.cart.domain.entity.Brand;
+
+/**
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 07-May-2011
+ * Time: 11:13:01
+ */
+public interface BrandService extends GenericService<Brand>{
+
+}
