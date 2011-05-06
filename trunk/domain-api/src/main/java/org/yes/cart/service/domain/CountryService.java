4304bb434b6d15482ffcd9b890b1ed2d2edf1484
azarny@gmail.com <azarny@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2011-05-06 15:19:38 +0000

    [YC-1] Domain api initial revision. 39

---
+++ trunk/domain-api/src/main/java/org/yes/cart/service/domain/CountryService.java
@@ -0,0 +1,12 @@
+package org.yes.cart.service.domain;
+
+import org.yes.cart.domain.entity.Country;
+
+/**
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 07-May-2011
+ * Time: 11:13:01
+ */
+public interface CountryService extends GenericService<Country> {
+    
+}
---
+++ trunk/domain-api/src/main/java/org/yes/cart/service/domain/CustomerWishListService.java
@@ -0,0 +1,22 @@
+package org.yes.cart.service.domain;
+
+import org.yes.cart.domain.entity.CustomerWishList;
+
+import java.util.List;
+
+/**
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 07-May-2011
+ * Time: 11:13:01
+ */
+public interface CustomerWishListService extends GenericService<CustomerWishList> {
+
+    /**
+     * Get customer credit cards.
+     * @param customerId customer id
+     * @return list of cards.
+     */
+    List<CustomerWishList> getByCustomerId(long customerId);
+
+
+}
