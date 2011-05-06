d49107c9cc307caacce20313ea67739fe1bf6c71
azarny@gmail.com <azarny@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2011-05-06 14:58:13 +0000

    [YC-1] Domain api initial revision. 17

---
+++ trunk/domain-api/src/main/java/org/yes/cart/domain/entity/AttrValueShop.java
@@ -0,0 +1,22 @@
+package org.yes.cart.domain.entity;
+
+/**
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 07-May-2011
+ * Time: 11:13:01
+ */
+public interface AttrValueShop extends AttrValue {
+
+    /**
+     * Get the shop.
+     * @return {@link Shop}
+     */
+    Shop getShop();
+
+    /**
+     * Set {@link Shop}
+     * @param shop {@link Shop} to set.
+     */
+    void setShop(Shop shop);
+
+}
