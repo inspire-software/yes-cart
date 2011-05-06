6a9fb19b617788256da2ac525a3ad58a1ed7e703
azarny <azarny@gmail.com>
2011-05-06 15:20:02 +0000

    [YC-1] Domain api initial revision. 40

---
+++ domain-api/src/main/java/org/yes/cart/service/domain/HashHelper.java
@@ -0,0 +1,24 @@
+package org.yes.cart.service.domain;
+
+import java.security.NoSuchAlgorithmException;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 07-May-2011
+ * Time: 11:13:01
+ */
+public interface HashHelper {
+
+    /**
+     * Get the string from given password.
+     *
+     * @param password given password
+     * @return md5 password string
+     * @throws java.security.NoSuchAlgorithmException     NoSuchAlgorithmException
+     * @throws java.io.UnsupportedEncodingException UnsupportedEncodingException
+     */
+    String getHash(String password) throws NoSuchAlgorithmException, UnsupportedEncodingException;
+
+
+}
