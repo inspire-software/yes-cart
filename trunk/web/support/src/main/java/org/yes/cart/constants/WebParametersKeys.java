211904cbd0c129b1570714f075826a70a05c6618
azarny@gmail.com <azarny@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2011-05-17 15:18:01 +0000

    [YC-1] Added web support mediation layer.

---
+++ trunk/web/support/src/main/java/org/yes/cart/constants/WebParametersKeys.java
@@ -0,0 +1,40 @@
+
+
+package org.yes.cart.constants;
+
+/**
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 2011-May-17
+ * Time: 11:54:55 AM
+ */
+public interface WebParametersKeys {
+
+    String SESSION_OBJECT_NAME = "requestRuntimeContainer";
+
+    String CATEGORY_ID = "category";
+
+    String PRODUCT_ID = "product";
+
+    String SKU_ID = "sku";
+
+    String CATEGORY_NAME = "categoryName";
+
+    String QUERY = "query";
+
+    String MAIN_PANEL_RENDERER = "renderer";
+
+    String PAGE = "page";
+
+    String SORT = "sorta";
+
+    String SORT_REVERSE = "sortd";
+    
+    String ADDRESS_ID = "addrId";
+
+    String ADDRESS_TYPE = "addrType";
+
+    /**
+     * Quantity of items per page.
+     */
+    String QUANTITY = "showItems";
+}
---
+++ trunk/web/support/src/main/java/org/yes/cart/request/HttpServletRequestWrapper.java
@@ -0,0 +1,50 @@
+
+
+package org.yes.cart.request;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * USed in multistore support. 
+ *
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 2011-May-17
+ * Time: 3:24:38 PM
+ *
+ * Wrapped servlet request.
+ *
+ */
+public class HttpServletRequestWrapper extends javax.servlet.http.HttpServletRequestWrapper {
+
+    private String servletPath;
+
+    /**
+     * Constructor.
+     * @param request  the <code>ServletRequest</code>
+     * @param servletPath resloved servlet path. See <code>ServletRequest#getServletPath</code>
+     */
+    public HttpServletRequestWrapper(final ServletRequest request, final String servletPath) {
+        super((HttpServletRequest) request);
+        this.servletPath = servletPath;
+    }
+
+    /**
+     * Constructor.
+     * @param request the <code>HttpServletRequest</code>
+     * @param servletPath resloved servlet path. See <code>ServletRequest#getServletPath</code>
+     */
+    public HttpServletRequestWrapper(final HttpServletRequest request, final String servletPath) {
+        super(request);
+        this.servletPath = servletPath;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getServletPath() {
+        return servletPath; 
+    }
+}
