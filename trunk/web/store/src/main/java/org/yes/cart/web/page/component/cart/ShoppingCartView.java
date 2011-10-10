570f06831c23288f65ebc12ac66e1bffbb9d604b
azarny@gmail.com <azarny@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2011-10-10 10:09:49 +0000

    [YC-1] - working on shopping cart

--- trunk/web/store/src/main/java/org/yes/cart/web/page/component/cart/ShoppingCartView.java
+++ trunk/web/store/src/main/java/org/yes/cart/web/page/component/cart/ShoppingCartView.java
@@ -2,8 +2,12 @@ package org.yes.cart.web.page.component.cart;
 
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.yes.cart.domain.misc.Pair;
 import org.yes.cart.web.application.ApplicationDirector;
 import org.yes.cart.web.page.component.BaseComponent;
+import org.yes.cart.web.page.component.price.PriceView;
+
+import java.math.BigDecimal;
 
 /**
  * User: Igor Azarny iazarny@yahoo.com
@@ -14,16 +18,19 @@ public class ShoppingCartView extends BaseComponent {
 
 
     // ------------------------------------- MARKUP IDs BEGIN ---------------------------------- //
-    private static final String SUB_TOTAL_LABEL = "subTotal";
+    private static final String SUB_TOTAL_VIEW = "subTotalView";
+    private static final String FEEDBACK_PANEL = "feedback";
+    private static final String ITEMS_LIST = "itemsList";
+    private static final String CART_FORM = "cartForm";
+
+
     private static final String DELIVERY_COST_LABEL = "deliveryCost";
     private static final String TAX_LABEL = "tax";
     private static final String TOTAL_LABEL = "total";
     private static final String GRAND_TOTAL_LABEL = "grandTotal";
-    private static final String FEEDBACK_PANEL = "feedback";
-    private static final String ITEMS_LIST = "itemsList";
     private static final String DELIVERY_LIST = "deliveryList";
     private static final String NEXT_LINK = "nextLink";
-    private static final String CART_FORM = "cartForm";
+
 
     private static final String MULTIPLE_DELIVERY_CHECKBOX = "multipleDelivery";
     private static final String MULTIPLE_DELIVERY_LABEL = "multipleDeliveryLabel";
@@ -42,18 +49,23 @@ public class ShoppingCartView extends BaseComponent {
     @Override
     protected void onBeforeRender() {
 
-        Form form = new Form(CART_FORM);
-
 
         add(
-                form
-        );
-
-        form.add(
-                new ShoppingCartItemsList(ITEMS_LIST, ApplicationDirector.getShoppingCart().getCartItemList())
-        );
-        form.add(
-                new FeedbackPanel(FEEDBACK_PANEL)
+                new Form(CART_FORM).add(
+                        new ShoppingCartItemsList(ITEMS_LIST, ApplicationDirector.getShoppingCart().getCartItemList())
+                ).add(
+                        new FeedbackPanel(FEEDBACK_PANEL)
+                ).add(
+                        new PriceView(
+                                SUB_TOTAL_VIEW,
+                                new Pair<BigDecimal, BigDecimal>(
+                                        ApplicationDirector.getShoppingCart().getCartSubTotal(
+                                                ApplicationDirector.getShoppingCart().getCartItemList()
+                                        ),
+                                        null),
+                                ApplicationDirector.getShoppingCart().getCurrencyCode(),
+                                true)
+                )
         );
 
 
