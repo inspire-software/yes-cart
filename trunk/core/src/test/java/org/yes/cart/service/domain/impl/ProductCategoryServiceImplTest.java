4daa4ca2600138b2a462389ddc2fbf07b2929736
plokhotnyuk@gmail.com <plokhotnyuk@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2011-11-05 16:29:16 +0000

    Renaming order depended tests to Test*.java pattern, while others will be *Test.java

--- trunk/core/src/test/java/org/yes/cart/service/domain/impl/ProductCategoryServiceImplTest.java
+++ trunk/core/src/test/java/org/yes/cart/service/domain/impl/ProductCategoryServiceImplTest.java
@@ -12,23 +12,17 @@ import static org.junit.Assert.assertEquals;
  * Date: 09-May-2011
  * Time: 14:12:54
  */
-public class TestProductCategoryServiceImpl extends BaseCoreDBTestCase {
+public class ProductCategoryServiceImplTest extends BaseCoreDBTestCase {
 
     @Test
     public void testGetProductById() {
-
         final ProductCategoryService productCategoryService =
                 (ProductCategoryService) ctx.getBean(ServiceSpringKeys.PRODUCT_CATEGORY_SERVICE);
-
         int rez = productCategoryService.getNextRank(211L);
         assertEquals("Next rank must be 450 for 211 category", 450, rez);
-
         rez = productCategoryService.getNextRank(-777L);
         assertEquals("Next rank must be 50 for unexisting -777 category", 50, rez);
-
         rez = productCategoryService.getNextRank(116L);
         assertEquals("Next rank must be 50 for existing -116 category without products", 50, rez);
-
-
     }
 }
