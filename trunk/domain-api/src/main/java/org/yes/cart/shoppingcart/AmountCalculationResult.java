d429daa825e95ba3157245b4e2253776d3d22e5b
azarny@gmail.com <azarny@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2011-12-01 21:07:01 +0000

    [YC-1] minor changes

--- trunk/domain-api/src/main/java/org/yes/cart/shoppingcart/AmountCalculationResult.java
+++ trunk/domain-api/src/main/java/org/yes/cart/shoppingcart/AmountCalculationResult.java
@@ -1,10 +1,124 @@
 package org.yes.cart.shoppingcart;
 
+import java.math.BigDecimal;
+
 /**
+ *
+ * Simple DTO class to hold amount calculation result.
+ *
  * Igor Azarny iazarny@yahoo.com
  * Date: 3-Dec-11
  * Time: 12:05
  */
 public interface AmountCalculationResult {
+
+    /**
+     * Get cart subtotal
+     * @return cart subtotal.
+     */
+    public BigDecimal getSubTotal();
+
+
+    /**
+     * Set sub total.
+     * @param subTotal sub total amount .
+     */
+    public void setSubTotal(BigDecimal subTotal);
+
+     /**
+     * Get sub total tax.
+     * @return sub total tax
+     */
+    public BigDecimal getSubTotalTax();
+
+     /**
+     * Get sub total tax.
+     * @param subTotalTax subtotal tax
+     */
+    public void setSubTotalTax(BigDecimal subTotalTax);
+
+    /**
+     * Get sub total amount including tax.
+     * @return  sub total amount including tax.
+     */
+    public BigDecimal getSubTotalAmount();
+
+    /**
+     * Set sub total amount
+     * @param subTotalAmount  sub total amount including tax.
+     */
+    public void setSubTotalAmount(BigDecimal subTotalAmount);
+
+    /**
+     * Get delivery amount.
+     * @return delivery amount.
+     */
+    public BigDecimal getDelivery();
+
+    /**
+     * Set delivery amount.
+     * @param delivery amount
+     */
+    public void setDelivery(BigDecimal delivery);
+
+    /**
+     * Get tax for delivery.
+     * @return delivery tax.
+     */
+    public BigDecimal getDeliveryTax();
+
+    /**
+     *
+     * @param deliveryTax set delivery tax
+     */
+    public void setDeliveryTax(BigDecimal deliveryTax);
+
+    /**
+     * Get delivery amount including tax.
+     * @return delivery amount including tax
+     */
+    public BigDecimal getDeliveryAmount();
+
+    /**
+     * Set delivery amount including tax.
+     * @param deliveryAmount delivery amount including tax
+     */
+    public void setDeliveryAmount(BigDecimal deliveryAmount);
+
+    /**
+     * Get total.
+     * @return total.
+     */
+    public BigDecimal getTotal();
+
+    /**
+     * Set total.
+     * @param total total to set.
+     */
+    public void setTotal(BigDecimal total);
+
+    /**
+     * Get total tax.
+     * @return total tax.
+     */
+    public BigDecimal getTotalTax();
+
+    /**
+     * Set total tax.
+     * @param totalTax tax to set.
+     */
+    public void setTotalTax(BigDecimal totalTax);
+
+    /**
+     * Get total amount.
+     * @return  total amount.
+     */
+    public BigDecimal getTotalAmount();
+
+    /**
+     * Set  total amount.
+     * @param totalAmount  total amount.
+     */
+    public void setTotalAmount(BigDecimal totalAmount);
 
 }
