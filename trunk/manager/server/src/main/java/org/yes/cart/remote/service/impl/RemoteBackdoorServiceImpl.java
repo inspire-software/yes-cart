210e34e7a22d0c4b22ec19fd58e4fe50255c186b
azarny@gmail.com <azarny@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2012-02-01 16:17:43 +0000

    [YC-0000] Adding remote tech services

---
+++ trunk/manager/server/src/main/java/org/yes/cart/remote/service/impl/RemoteBackdoorServiceImpl.java
@@ -0,0 +1,77 @@
+package org.yes.cart.remote.service.impl;
+
+import flex.messaging.FlexContext;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.yes.cart.remote.service.RemoteBackdoorService;
+import org.yes.cart.web.service.ws.BackdoorService;
+import org.yes.cart.web.service.ws.client.BackdoorServiceClientFactory;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import java.util.List;
+
+/**
+ * User: iga Igor Azarny
+ * Date: 4 - feb - 12
+ * Time: 5:44 PM
+ */
+public class RemoteBackdoorServiceImpl implements RemoteBackdoorService {
+
+
+    /** {@inheritDoc} */
+    public int reindexAllProducts() {
+        return getBackdoorService().reindexAllProducts();
+    }
+
+    /** {@inheritDoc} */
+    public int reindexProduct(final long productPk) {
+        return getBackdoorService().reindexProduct(productPk);
+    }
+
+    /** {@inheritDoc} */
+    public int reindexProducts(final long[] productPks) {
+        return getBackdoorService().reindexProducts(productPks);
+    }
+
+    /** {@inheritDoc} */
+    public List<Object[]> sqlQuery(final String query) {
+        return getBackdoorService().sqlQuery(query);
+    }
+
+    /** {@inheritDoc} */
+    public List<Object[]> hsqlQuery(final String query) {
+        return getBackdoorService().hsqlQuery(query);
+    }
+
+    /** {@inheritDoc} */
+    public List<Object[]> luceneQuery(final String query) {
+        return getBackdoorService().luceneQuery(query);
+    }
+
+    private BackdoorServiceClientFactory backdoorServiceClientFactory = null;
+
+    private synchronized BackdoorServiceClientFactory getBackdoorServiceClientFactory() {
+
+        if (backdoorServiceClientFactory == null) {
+            backdoorServiceClientFactory = new BackdoorServiceClientFactory();
+        }
+
+        return backdoorServiceClientFactory;
+
+    }
+
+    private BackdoorService getBackdoorService() {
+
+        String userName =  ((UsernamePasswordAuthenticationToken) FlexContext.getUserPrincipal()).getName();
+        String password = (String) ((UsernamePasswordAuthenticationToken) FlexContext.getUserPrincipal()).getCredentials();
+
+        return getBackdoorServiceClientFactory().getBackdoorService(
+                userName,
+                password,
+                "http://localhost:8080/yes-shop/services/backdoor");
+
+    }
+
+
+}
