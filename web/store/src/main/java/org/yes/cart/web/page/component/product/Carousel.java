aa7c921defbf7ee9616658d1ea03d7494468a6b9
azarny <azarny@gmail.com>
2012-03-08 19:04:16 +0000

    [YC-0000] light default default design in progress

---
+++ web/store/src/main/java/org/yes/cart/web/page/component/product/Carousel.java
@@ -0,0 +1,20 @@
+package org.yes.cart.web.page.component.product;
+
+/**
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 3/8/12
+ * Time: 8:52 PM
+ */
+public class Carousel extends  FeaturedProducts {
+
+    /**
+     * Construct product list to show.
+     *
+     * @param id component id.
+     */
+    public Carousel(final String id) {
+        super(id);
+    }
+
+
+}
