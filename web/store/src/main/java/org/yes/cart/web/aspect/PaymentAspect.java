f5229358a280441d81d9ef69560c84920bc672c1
azarny <azarny@gmail.com>
2012-04-06 14:12:49 +0000

    [YC-0000] Notification aspects

--- web/store/src/main/java/org/yes/cart/web/aspect/PaymentAspect.java
+++ web/store/src/main/java/org/yes/cart/web/aspect/PaymentAspect.java
@@ -6,8 +6,15 @@ import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jms.core.JmsTemplate;
+import org.yes.cart.domain.entity.CustomerOrder;
+import org.yes.cart.domain.message.consumer.StandardMessageListener;
+import org.yes.cart.domain.misc.Pair;
 import org.yes.cart.service.domain.aspect.impl.BaseNotificationAspect;
 import org.yes.cart.util.ShopCodeContext;
+import org.yes.cart.web.application.ApplicationDirector;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Aspect responsible to send notifications about
@@ -34,13 +41,35 @@ public class PaymentAspect extends BaseNotificationAspect {
 
     /**
      * Perform shopper notification, about payment authorize.
-     * @param pjp
+     * @param pjp    {@link ProceedingJoinPoint}
      * @return result of original operation.
+     * @throws re throws exception
      */
     @Around("execution(* org.yes.cart.service.payment.impl.PaymentProcessorImpl.authorize(..))")
     public Object doAuthorize(final ProceedingJoinPoint pjp) throws Throwable {
-        final Object rez =  pjp.proceed();
-        sendNotification((String)rez);
+        final String rez =  (String) pjp.proceed();
+        
+        sendNotification(
+
+                new HashMap<String, Object>() {{
+
+                    final CustomerOrder customerOrder = (CustomerOrder) pjp.getArgs()[0];
+                    put(StandardMessageListener.SHOP_CODE, ShopCodeContext.getShopCode());
+                    put(StandardMessageListener.CUSTOMER_EMAIL, customerOrder.getCustomer().getEmail());
+                    put(StandardMessageListener.RESULT, rez);
+                    put(StandardMessageListener.DEFAULT, customerOrder);
+                    put(StandardMessageListener.TEMPLATE_FOLDER, ApplicationDirector.getCurrentMailTemplateFolder());
+
+                    put(StandardMessageListener.SHOP, ApplicationDirector.getCurrentShop());
+                    put(StandardMessageListener.CUSTOMER, customerOrder.getCustomer());
+
+                    put(StandardMessageListener.TEMPLATE_NAME, "payment");
+
+
+                }}
+
+        );
+
         return rez;
     }
 }
