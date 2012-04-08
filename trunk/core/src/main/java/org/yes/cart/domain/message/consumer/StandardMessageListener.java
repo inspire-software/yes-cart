1692c56392e62258b490f7b1036807a414e668f3
azarny@gmail.com <azarny@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2012-04-08 13:01:11 +0000

    [YC-0000] email notifications

--- trunk/core/src/main/java/org/yes/cart/domain/message/consumer/StandardMessageListener.java
+++ trunk/core/src/main/java/org/yes/cart/domain/message/consumer/StandardMessageListener.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.yes.cart.domain.entity.Shop;
+import org.yes.cart.service.domain.CustomerService;
+import org.yes.cart.service.domain.ShopService;
 import org.yes.cart.service.mail.MailComposer;
 import org.yes.cart.util.ShopCodeContext;
 
@@ -16,12 +18,12 @@ import java.util.Map;
 /**
  * Standard message listener, which get the message, extract shopper id, enrich context with customer object
  * and perform mail notification within specified mail template.
- *
+ * <p/>
  * User: Igor Azarny iazarny@yahoo.com
  * Date: 4/7/12
  * Time: 4:12 PM
  */
-public class StandardMessageListener  implements MessageListener {
+public class StandardMessageListener implements MessageListener {
 
     /**
      * Shop code.  Email context variable.
@@ -44,15 +46,26 @@ public class StandardMessageListener  implements MessageListener {
     public static final String CUSTOMER = "customer";
 
     /**
-     * Result.  Email context variable.
+     * Result.  Result context variable.
      */
     public static final String RESULT = "result";
 
-    /** Default object, which passed to email template.  */
-    public static final String DEFAULT = "default";
-    /** Current template folder. */
+    /**
+     * All parameters will be passed with index param0, param1, etc
+     */
+    public static final String PARAM_PREFIX = "param";
+
+    /**
+     * Default object, which passed to email template.
+     */
+    public static final String ROOT = "root";
+    /**
+     * Current template folder.
+     */
     public static final String TEMPLATE_FOLDER = "templateFolder";
-    /** Template folder. */
+    /**
+     * Template folder.
+     */
     public static final String TEMPLATE_NAME = "templateName";
 
 
@@ -62,19 +75,30 @@ public class StandardMessageListener  implements MessageListener {
 
     private final MailComposer mailComposer;
 
+    private final CustomerService customerService;
+
+    private final ShopService shopService;
+
     /**
      * Contruct jms listener.
      *
-     * @param javaMailSender mail sender to use.
-     * @param mailComposer   mail composer
+     * @param javaMailSender  mail sender to use.
+     * @param mailComposer    mail composer
+     * @param shopService     shop service
+     * @param customerService customer service
      */
     public StandardMessageListener(
             final JavaMailSender javaMailSender,
-            final MailComposer mailComposer) {
+            final MailComposer mailComposer,
+            final CustomerService customerService,
+            final ShopService shopService) {
         this.javaMailSender = javaMailSender;
         this.mailComposer = mailComposer;
+        this.shopService = shopService;
+        this.customerService = customerService;
+
     }
-    
+
 
     /**
      * {@inheritDoc}
@@ -93,12 +117,11 @@ public class StandardMessageListener  implements MessageListener {
             }
 
 
-
             final MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 
             mailComposer.composeMessage(
                     mimeMessage,
-                    null,
+                    (String) map.get(SHOP_CODE),
                     (String) map.get(TEMPLATE_FOLDER),
                     (String) map.get(TEMPLATE_NAME),
                     null,//must be from properties "todo@getfromsho.com", //((Shop)map.get(SHOP)).getAttribute()
@@ -110,9 +133,8 @@ public class StandardMessageListener  implements MessageListener {
             javaMailSender.send(mimeMessage);
 
 
-
         } catch (Exception e) {
-            LOG.error("Cant cast object message body to expected format " , message);
+            LOG.error("Cant cast object message body to expected format ", message);
         }
 
 
@@ -120,18 +142,25 @@ public class StandardMessageListener  implements MessageListener {
 
     /**
      * Enrich given map with shopper object.
+     *
      * @param map givem map to enrich
      */
     private void enrichMapWithCustomer(final Map<String, Object> map) {
+        map.put(CUSTOMER,
+                customerService.findCustomer((String) map.get(CUSTOMER_EMAIL)));
 
 
     }
 
     /**
      * Enrich given map with shop object.
+     *
      * @param map givem map to enrich
      */
     private void enrichMapWithShop(final Map<String, Object> map) {
+
+        map.put(SHOP,
+                shopService.getShopByCode((String) map.get(SHOP_CODE)));
 
 
     }
--- trunk/web/store/src/main/java/org/yes/cart/web/aspect/PaymentAspect.java
+++ trunk/web/store/src/main/java/org/yes/cart/web/aspect/PaymentAspect.java
@@ -8,20 +8,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.jms.core.JmsTemplate;
 import org.yes.cart.domain.entity.CustomerOrder;
 import org.yes.cart.domain.message.consumer.StandardMessageListener;
-import org.yes.cart.domain.misc.Pair;
 import org.yes.cart.service.domain.aspect.impl.BaseNotificationAspect;
 import org.yes.cart.util.ShopCodeContext;
 import org.yes.cart.web.application.ApplicationDirector;
 
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Aspect responsible to send notifications about
  * User: iga Igor Azarny
  * Date: 7 Apr 2012
  * Time: 4:41 PM
- *
  */
 @Aspect
 public class PaymentAspect extends BaseNotificationAspect {
@@ -30,46 +27,94 @@ public class PaymentAspect extends BaseNotificationAspect {
 
     /**
      * Construct aspect.
+     *
      * @param jmsTemplate what jms use to send notification.
      */
     public PaymentAspect(final JmsTemplate jmsTemplate) {
         super(jmsTemplate);
     }
-    
-    
-    
+
 
     /**
      * Perform shopper notification, about payment authorize.
-     * @param pjp    {@link ProceedingJoinPoint}
+     *
+     * @param pjp {@link ProceedingJoinPoint}
      * @return result of original operation.
-     * @throws re throws exception
+     * @throws Throwable re throws exception
      */
     @Around("execution(* org.yes.cart.service.payment.impl.PaymentProcessorImpl.authorize(..))")
     public Object doAuthorize(final ProceedingJoinPoint pjp) throws Throwable {
-        final String rez =  (String) pjp.proceed();
-        
-        sendNotification(
+        final String rez = (String) pjp.proceed();
 
-                new HashMap<String, Object>() {{
+        final HashMap<String, Object> map = new HashMap<String, Object>();
 
-                    final CustomerOrder customerOrder = (CustomerOrder) pjp.getArgs()[0];
-                    put(StandardMessageListener.SHOP_CODE, ShopCodeContext.getShopCode());
-                    put(StandardMessageListener.CUSTOMER_EMAIL, customerOrder.getCustomer().getEmail());
-                    put(StandardMessageListener.RESULT, rez);
-                    put(StandardMessageListener.DEFAULT, customerOrder);
-                    put(StandardMessageListener.TEMPLATE_FOLDER, ApplicationDirector.getCurrentMailTemplateFolder());
+        fillParameters(pjp, map);
 
-                    put(StandardMessageListener.SHOP, ApplicationDirector.getCurrentShop());
-                    put(StandardMessageListener.CUSTOMER, customerOrder.getCustomer());
+        final CustomerOrder customerOrder = (CustomerOrder) pjp.getArgs()[0];
+        map.put(StandardMessageListener.SHOP_CODE, ShopCodeContext.getShopCode());
+        map.put(StandardMessageListener.CUSTOMER_EMAIL, customerOrder.getCustomer().getEmail());
+        map.put(StandardMessageListener.RESULT, rez);
+        map.put(StandardMessageListener.ROOT, customerOrder);
+        map.put(StandardMessageListener.TEMPLATE_FOLDER, ApplicationDirector.getCurrentMailTemplateFolder());
 
-                    put(StandardMessageListener.TEMPLATE_NAME, "payment");
+        map.put(StandardMessageListener.SHOP, ApplicationDirector.getCurrentShop());
+        map.put(StandardMessageListener.CUSTOMER, customerOrder.getCustomer());
+
+        map.put(StandardMessageListener.TEMPLATE_NAME, "payment");
 
 
-                }}
-
-        );
+        sendNotification(map);
 
         return rez;
+    }
+
+
+    /**
+     * Perform shopper notification, about payment authorize.
+     *
+     * @param pjp {@link ProceedingJoinPoint}
+     * @return result of original operation.
+     * @throws Throwable re throws exception
+     */
+    @Around("execution(* org.yes.cart.service.payment.impl.PaymentProcessorImpl.shipmentComplete(..))")
+    public Object doShipmentComplete(final ProceedingJoinPoint pjp) throws Throwable {
+
+        final String rez = (String) pjp.proceed();
+
+        final HashMap<String, Object> map = new HashMap<String, Object>();
+
+        fillParameters(pjp, map);
+
+        final CustomerOrder customerOrder = (CustomerOrder) pjp.getArgs()[0];
+        map.put(StandardMessageListener.SHOP_CODE, ShopCodeContext.getShopCode());
+        map.put(StandardMessageListener.CUSTOMER_EMAIL, customerOrder.getCustomer().getEmail());
+        map.put(StandardMessageListener.RESULT, rez);
+        map.put(StandardMessageListener.ROOT, customerOrder);
+        map.put(StandardMessageListener.TEMPLATE_FOLDER, ApplicationDirector.getCurrentMailTemplateFolder());
+
+        map.put(StandardMessageListener.SHOP, ApplicationDirector.getCurrentShop());
+        map.put(StandardMessageListener.CUSTOMER, customerOrder.getCustomer());
+
+        map.put(StandardMessageListener.TEMPLATE_NAME, "shipmentComplete");
+
+
+        sendNotification(map);
+
+        return rez;
+
+    }
+
+    /**
+     * Fill all passed parameters into message map.
+     *
+     * @param pjp {@link ProceedingJoinPoint}
+     * @param map contxt map
+     */
+    private void fillParameters(final ProceedingJoinPoint pjp, final HashMap<String, Object> map) {
+        if (pjp.getArgs() != null) {
+            for (int i = 0; i < pjp.getArgs().length; i++) {
+                map.put(StandardMessageListener.PARAM_PREFIX + i, pjp.getArgs()[i]);
+            }
+        }
     }
 }
