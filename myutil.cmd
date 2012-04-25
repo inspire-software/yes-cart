17168ea49c9ff8a79e3234aa2cdc3cc86a1e187d
azarny <azarny@gmail.com>
2012-04-25 15:24:01 +0000

    -

--- myutil.cmd
+++ myutil.cmd
@@ -1,8 +1,16 @@
-rem mvn install -DskipTests=true -Pmysql
-xcopy /i /f /e D:\dev\yes-cart\manager\server\target\yes-manager D:\dev\apache-tomcat-7.0.14\webapps\yes-manager
-xcopy /i /f /e D:\dev\yes-cart\web\store\target\yes-shop D:\dev\apache-tomcat-7.0.14\webapps\yes-shop
+rem call mvn clean install -DskipTests=true 
+rem -Pmysql
+
+rd /q/s D:\dev\apache-tomcat-7.0.23\webapps\yes-manager
+rd /q/s D:\dev\apache-tomcat-7.0.23\webapps\yes-shop
+
+mkdir D:\dev\apache-tomcat-7.0.23\webapps\yes-manager
+mkdir D:\dev\apache-tomcat-7.0.23\webapps\yes-shop
+
+xcopy /i /f /e D:\dev\yes-cart\manager\server\target\yes-manager D:\dev\apache-tomcat-7.0.23\webapps\yes-manager
+xcopy /i /f /e D:\dev\yes-cart\web\store\target\yes-shop D:\dev\apache-tomcat-7.0.23\webapps\yes-shop
 
 cd D:\dev\yes-cart\manager\client
 call ant
 cd D:\dev\yes-cart
-copy D:\dev\yes-cart\manager\client\target\en_US\ShopManagerApplication.swf D:\dev\apache-tomcat-7.0.14\webapps\yes-manager\ShopManagerApplication.swf
+copy D:\dev\yes-cart\manager\client\target\en_US\ShopManagerApplication.swf D:\dev\apache-tomcat-7.0.23\webapps\yes-manager\ShopManagerApplication.swf
