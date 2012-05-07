8736b6a7d9502d81bfeb9e8838da124b3893c6d5
azarny@gmail.com <azarny@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2012-05-07 19:34:37 +0000

    [YC-0000] icecat transformer

---
+++ trunk/util/icecat/src/main/groovy/org/yes/cart/icecat/transform/CategoryWalker.groovy
@@ -0,0 +1,21 @@
+package org.yes.cart.icecat.transform
+
+/**
+ * User: Igor Azarny iazarny@yahoo.com
+ * Date: 5/7/12
+ * Time: 10:11 PM
+ */
+class CategoryWalker {
+
+    Context context;
+
+    CategoryWalker(Context context) {
+
+        this.context = context;
+
+    }
+
+
+
+
+}
---
+++ trunk/util/icecat/src/main/groovy/org/yes/cart/icecat/transform/Context.groovy
@@ -0,0 +1,22 @@
+package org.yes.cart.icecat.transform
+
+/**
+ * Context Groovy class.
+ */
+class Context  {
+
+    String url = "http://data.icecat.biz/export/freexml.int/";
+
+    String login;
+
+    String pwd;
+
+    int langId;
+
+    String categories;
+
+    long mindata;
+
+    int limit = 1000;
+
+}
