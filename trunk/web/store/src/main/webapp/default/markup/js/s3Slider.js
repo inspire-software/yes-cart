7922a2a4c83ae7754275f9b59f86a6b12d8012e3
denis.v.pavlov@gmail.com <denis.v.pavlov@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2012-07-15 00:39:53 +0000

    [ YC-23 ] Update license info in all files

--- trunk/web/store/src/main/webapp/default/markup/js/s3Slider.js
+++ trunk/web/store/src/main/webapp/default/markup/js/s3Slider.js
@@ -1,13 +1,18 @@
-/* ------------------------------------------------------------------------
-	s3Slider
-	
-	Developped By: Boban Karišik -> http://www.serie3.info/
-        CSS Help: Mészáros Róbert -> http://www.perspectived.com/
-	Version: 1.0
-	
-	Copyright: Feel free to redistribute the script/modify it, as
-			   long as you leave my infos at the top.
-------------------------------------------------------------------------- */
+/*
+ * Copyright 2009 Igor Azarnyi, Denys Pavlov
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 
 
 (function($){  
