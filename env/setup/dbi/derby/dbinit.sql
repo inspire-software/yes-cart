7692e6025fc69c7343a1da6a4d8f31929e3fcc87
Denys Pavlov <denyspavlov@users.noreply.github.com>
2012-08-17 13:58:27 +0000

    [ YC-5 ] Tidy up build/setup process

--- env/setup/dbi/derby/dbinit.sql
+++ env/setup/dbi/derby/dbinit.sql
@@ -2,11 +2,11 @@
 Initialisation SQL for YesCart
 @author Denys Pavlov
  */
-connect 'jdbc:derby://localhost:1527/yes;create=true';
+connect 'jdbc:derby://localhost:1527/yes;create=true;unicode=true';
 run '../../../persistence/sql/resources/derby/create-tables.sql';
 run '../../../env/setup/dbi/initdata.sql';
 disconnect;
-connect 'jdbc:derby://localhost:1527/yespay;create=true';
+connect 'jdbc:derby://localhost:1527/yespay;create=true;unicode=true';
 run '../../../core-modules/core-module-payment-base/src/main/resources/sql/derby/create-tables.sql';
 run '../../../core-modules/core-module-payment-base/src/main/resources/sql/payinitdata.sql';
 run '../../../core-modules/core-module-payment-capp/src/main/resources/sql/payinitdata.sql';
