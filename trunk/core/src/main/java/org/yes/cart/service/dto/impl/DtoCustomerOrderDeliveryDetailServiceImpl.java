43cc0e78b7f4159ad647d8e9c1b1e194ce69040c
azarny@gmail.com <azarny@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2012-12-16 18:23:07 +0000

    [YC-78] UI for edit delivery

--- trunk/core/src/main/java/org/yes/cart/service/dto/impl/DtoCustomerOrderDeliveryDetailServiceImpl.java
+++ trunk/core/src/main/java/org/yes/cart/service/dto/impl/DtoCustomerOrderDeliveryDetailServiceImpl.java
@@ -5,6 +5,8 @@ import org.yes.cart.domain.dto.CustomerOrderDeliveryDetailDTO;
 import org.yes.cart.domain.dto.factory.DtoFactory;
 import org.yes.cart.domain.dto.impl.CustomerOrderDeliveryDetailDTOImpl;
 import org.yes.cart.domain.entity.CustomerOrderDeliveryDet;
+import org.yes.cart.exception.UnableToCreateInstanceException;
+import org.yes.cart.exception.UnmappedInterfaceException;
 import org.yes.cart.service.domain.GenericService;
 import org.yes.cart.service.dto.DtoCustomerOrderDeliveryDetailService;
 
@@ -43,4 +45,5 @@ public class DtoCustomerOrderDeliveryDetailServiceImpl extends AbstractDtoServic
     public Class<CustomerOrderDeliveryDet> getEntityIFace() {
         return CustomerOrderDeliveryDet.class;
     }
+
 }
