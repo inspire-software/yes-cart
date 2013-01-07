8f0302d54433059c6d79811914d5513bee766355
azarny <azarny@gmail.com>
2013-01-07 20:56:45 +0000

    [YC-30] Working on non integer product qty.

--- core/src/main/java/org/yes/cart/shoppingcart/impl/CartItemImpl.java
+++ core/src/main/java/org/yes/cart/shoppingcart/impl/CartItemImpl.java
@@ -75,9 +75,9 @@ public class CartItemImpl implements CartItem {
     public void setQuantity(final BigDecimal quantity) {
         //this.quantity = new BigDecimal(MoneyUtils.notNull(quantity, DEFAULT_QUANTITY).intValue());
         this.quantity = MoneyUtils.notNull(quantity, DEFAULT_QUANTITY);
-        if (!MoneyUtils.isFirstBiggerThanOrEqualToSecond(this.quantity, BigDecimal.ONE)) {
+        /*if (!MoneyUtils.isFirstBiggerThanOrEqualToSecond(this.quantity, BigDecimal.ONE)) {
             this.quantity = DEFAULT_QUANTITY;
-        }
+        } */
     }
 
     /**
