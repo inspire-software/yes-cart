4817cab34510052eb3ab6b93d522f8ffeb8996cc
azarny@gmail.com <azarny@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2013-05-26 10:20:22 +0000

    [yc-0000] transactional model reworking . all tests are passed

--- branches/tr/core/src/test/java/org/yes/cart/service/domain/impl/TestWarehouseServiceImpl.java
+++ branches/tr/core/src/test/java/org/yes/cart/service/domain/impl/TestWarehouseServiceImpl.java
@@ -43,7 +43,6 @@ public class TestWarehouseServiceImpl extends BaseCoreDBTestCase {
     private WarehouseService warehouseService;
     private ShopService shopService;
     private EntityFactory entityFactory;
-    private GenericDAO<ShopWarehouse, Long> shopWarehouseDao;
     private Shop shop;
     private Warehouse warehouse;
 
@@ -52,7 +51,6 @@ public class TestWarehouseServiceImpl extends BaseCoreDBTestCase {
         shopService = (ShopService) ctx().getBean(ServiceSpringKeys.SHOP_SERVICE);
         warehouseService = (WarehouseService) ctx().getBean(ServiceSpringKeys.WAREHOUSE_SERVICE);
         entityFactory = warehouseService.getGenericDao().getEntityFactory();
-        shopWarehouseDao = (GenericDAO) ctx().getBean(DaoServiceBeanKeys.SHOP_WAREHOUSE_DAO);
         super.setUp();
     }
 
@@ -93,7 +91,7 @@ public class TestWarehouseServiceImpl extends BaseCoreDBTestCase {
         assertEquals("Test default rank", 100, shopWarehouse.getRank());
 
         warehouseService.updateShopWarehouseRank(shopWarehouse.getShopWarehouseId(), 200);
-        shopWarehouse = shopWarehouseDao.findById(shopWarehouse.getShopWarehouseId());
+        shopWarehouse = warehouseService.findShopWarehouseById(shopWarehouse.getShopWarehouseId());
         assertEquals("Test default rank", 200, shopWarehouse.getRank());
     }
 
