9ec65f62e184f99d8bfa722844fbe911118a7770
azarny@gmail.com <azarny@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2013-10-18 13:58:10 +0000

    [YC-225] Applying responsive design

--- branches/responsive/web/store/src/main/webapp/default/markup/js/menu.js
+++ branches/responsive/web/store/src/main/webapp/default/markup/js/menu.js
@@ -13,6 +13,12 @@ $(document).ready(function() {
 		$(this).toggleClass("active");
 		$(".nav").toggle();
 	});
+
+	$(".toggleMenu2").click(function(e) {
+		e.preventDefault();
+		$(this).toggleClass("active");
+		$(".topcatnav").toggle();
+	});
 	adjustMenu();
 })
 
@@ -21,30 +27,38 @@ $(window).bind('resize orientationchange', function() {
 	adjustMenu();
 });
 
+function adjustMenuSmall(styleName, toggleMenuStyleName) {
+    $(toggleMenuStyleName).css("display", "inline-block");
+    if (!$(toggleMenuStyleName).hasClass("active")) {
+        $(styleName).hide();
+    } else {
+        $(styleName).show();
+    }
+    $(styleName + " li").unbind('mouseenter mouseleave');
+    $(styleName + " li a.parent").unbind('click').bind('click', function (e) {
+        // must be attached to anchor element to prevent bubbling
+        e.preventDefault();
+        $(this).parent("li").toggleClass("hover");
+    });
+}
+
+function adjustMenuBig(styleName, toggleMenuStyleName) {
+    $(toggleMenuStyleName).css("display", "none");
+    $(styleName).show();
+    $(styleName + " li").removeClass("hover");
+    $(styleName + " li a").unbind('click');
+    $(styleName + " li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function () {
+        // must be attached to li so that mouseleave is not triggered when hover over submenu
+        $(this).toggleClass('hover');
+    });
+}
 var adjustMenu = function() {
 	if (ww < 768) {
-		$(".toggleMenu").css("display", "inline-block");
-		if (!$(".toggleMenu").hasClass("active")) {
-			$(".nav").hide();
-		} else {
-			$(".nav").show();
-		}
-		$(".nav li").unbind('mouseenter mouseleave');
-		$(".nav li a.parent").unbind('click').bind('click', function(e) {
-			// must be attached to anchor element to prevent bubbling
-			e.preventDefault();
-			$(this).parent("li").toggleClass("hover");
-		});
-	} 
-	else if (ww >= 768) {
-		$(".toggleMenu").css("display", "none");
-		$(".nav").show();
-		$(".nav li").removeClass("hover");
-		$(".nav li a").unbind('click');
-		$(".nav li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
-		 	// must be attached to li so that mouseleave is not triggered when hover over submenu
-		 	$(this).toggleClass('hover');
-		});
-	}
+        adjustMenuSmall(".nav", ".toggleMenu");
+        adjustMenuSmall(".topcatnav", ".toggleMenu2");
+    } else if (ww >= 768) {
+        adjustMenuBig(".nav", ".toggleMenu");
+        adjustMenuBig(".topcatnav", ".toggleMenu2");
+    }
 }
 
