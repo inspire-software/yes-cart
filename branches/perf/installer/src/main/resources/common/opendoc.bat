59eaf3525f43d2deac599a34902cdf0eddd8fab1
azarny@gmail.com <azarny@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2013-11-20 08:02:27 +0000

    delete performance branch

--- branches/perf/installer/src/main/resources/common/opendoc.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rem
-rem YesCart. Open Yes-Cart documentation
-rem @author Igor Azarny (iazarny@yahoo.com)
-
-start http://code.google.com/p/yes-cart
-
-exit
--- branches/perf/util/devnullsmtp/readme.txt
+++
@@ -1,2 +0,0 @@
-Use for test purposes on localhost. 
-Please, dont run tests, that use dumbster where dev null server is listenin on 25 port
