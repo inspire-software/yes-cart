59966a1ec7158417f3fea104c6822bd3f36f1dc1
azarny@gmail.com <azarny@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2013-11-20 08:02:43 +0000

    delete 204 branch

--- branches/204/core/src/test/java/org/yes/cart/service/order/impl/PreOrderJobImplTest.java
+++
@@ -1,9 +0,0 @@
-package org.yes.cart.service.order.impl;
-
-import org.yes.cart.BaseCoreDBTestCase;
-
-/**
- * Test for pre ordered products processing
- */
-public class PreOrderJobImplTest  extends BaseCoreDBTestCase {
-}
