95f8f15503624ef89016a2aee93dbbbec562eb4a
denislozenko <denis.lozenko@gmail.com>
2014-06-12 09:15:22 +0000

    merge -r 1319 [ YC-379 ] Create and add wish list Items (some improvement to adding wish list items, carry http params)

--- web/store/src/main/java/org/yes/cart/web/page/component/customer/wishlist/WishListItemAddPage.java
+++ web/store/src/main/java/org/yes/cart/web/page/component/customer/wishlist/WishListItemAddPage.java
@@ -7,12 +7,14 @@ import org.apache.wicket.authroles.authorization.strategies.role.annotations.Aut
 import org.apache.wicket.protocol.https.RequireHttps;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.springframework.web.util.WebUtils;
 import org.yes.cart.constants.ServiceSpringKeys;
 import org.yes.cart.domain.entity.ProductSku;
 import org.yes.cart.service.domain.ProductSkuService;
 import org.yes.cart.shoppingcart.ShoppingCartCommand;
 import org.yes.cart.web.page.AbstractWebPage;
 import org.yes.cart.web.support.constants.WebParametersKeys;
+import org.yes.cart.web.util.WicketUtil;
 
 /**
  * User: denispavlov
@@ -46,15 +48,15 @@ public class WishListItemAddPage extends AbstractWebPage {
         super.onBeforeRender();
         persistCartIfNecessary();
 
-        final PageParameters targetParams = new PageParameters();
+        final PageParameters targetParams = WicketUtil.getFilteredRequestParameters(params);
 
         if (StringUtils.isNotBlank(skuCode)) {
 
             final ProductSku sku = productSkuService.getProductSkuBySkuCode(skuCode);
             targetParams.set(WebParametersKeys.SKU_ID, sku.getSkuId());
+            targetParams.set(WebParametersKeys.WISHLIST_ITEM_ADDED, skuCode);
 
         }
-        targetParams.set(WebParametersKeys.WISHLIST_ITEM_ADDED, skuCode);
         throw new RestartResponseException(Application.get().getHomePage(), targetParams);
 
     }
