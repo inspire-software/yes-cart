7cc3737acea53f333eb1b54e0ef1b43f127ff70e
denis.v.pavlov@gmail.com <denis.v.pavlov@gmail.com@b5e963ab-246d-477c-a38c-82af0a68ca80>
2015-03-03 23:19:35 +0000

    [ YC-509 ] Update quantity picker information after successfull add to cart operation

--- trunk/web/store/src/main/java/org/yes/cart/web/page/AjaxAtbPage.java
+++ trunk/web/store/src/main/java/org/yes/cart/web/page/AjaxAtbPage.java
@@ -1,10 +1,22 @@
 package org.yes.cart.web.page;
 
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.tree.LabelIconPanel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.util.string.*;
+import org.yes.cart.domain.entity.ProductQuantityModel;
+import org.yes.cart.domain.entity.ProductSku;
+import org.yes.cart.shoppingcart.ShoppingCart;
 import org.yes.cart.shoppingcart.ShoppingCartCommand;
+import org.yes.cart.web.application.ApplicationDirector;
 import org.yes.cart.web.page.component.cart.SmallShoppingCartView;
+import org.yes.cart.web.support.constants.StorefrontServiceSpringKeys;
+import org.yes.cart.web.support.service.ProductServiceFacade;
+
+import java.math.BigDecimal;
 
 /**
  * User: denispavlov
@@ -12,6 +24,9 @@ import org.yes.cart.web.page.component.cart.SmallShoppingCartView;
  * Time: 7:16 PM
  */
 public class AjaxAtbPage extends AbstractWebPage {
+
+    @SpringBean(name = StorefrontServiceSpringKeys.PRODUCT_SERVICE_FACADE)
+    protected ProductServiceFacade productServiceFacade;
 
     public AjaxAtbPage(final PageParameters params) {
         super(params);
@@ -25,9 +40,50 @@ public class AjaxAtbPage extends AbstractWebPage {
 
         executeHttpPostedCommands();
 
+        final StringValue skuValue = getPageParameters().get(ShoppingCartCommand.CMD_ADDTOCART);
+        final String sku = skuValue.toString();
+
         addOrReplace(new SmallShoppingCartView("smallCart"));
         addOrReplace(new Label("productAddedMsg",
-                new StringResourceModel("itemAdded", this, null, getPageParameters().get(ShoppingCartCommand.CMD_ADDTOCART))));
+                new StringResourceModel("itemAdded", this, null, skuValue)));
+
+        final ProductSku productSku = productServiceFacade.getProductSkuBySkuCode(sku);
+        final ShoppingCart cart = ApplicationDirector.getShoppingCart();
+        final BigDecimal cartQty = cart.getProductSkuQuantity(sku);
+        final ProductQuantityModel pqm = productServiceFacade.getProductQuantity(cartQty, productSku.getProduct());
+
+        final String message;
+        if (!pqm.canOrderMore()) {
+            message = getLocalizer().getString("quantityPickerFullTooltip", this,
+                    new Model<Object[]>(new Object[] {
+                            pqm.getCartQty().toPlainString()
+                    }));
+        } else if (pqm.hasMax()) {
+            message = getLocalizer().getString("quantityPickerTooltip", this,
+                    new Model<Object[]>(new Object[] {
+                            pqm.getMin().toPlainString(),
+                            pqm.getStep().toPlainString(),
+                            pqm.getMax().toPlainString(),
+                            pqm.getCartQty().toPlainString()
+                    }));
+        } else {
+            message = getLocalizer().getString("quantityPickerTooltipNoMax", this,
+                    new Model<Object[]>(new Object[] {
+                            pqm.getMin().toPlainString(),
+                            pqm.getStep().toPlainString(),
+                            pqm.getCartQty().toPlainString()
+                    }));
+        }
+
+        final StringBuilder outJson = new StringBuilder();
+        outJson.append("{ \"SKU\": \"").append(sku.replace("\"", "")).append("\"")
+                .append(", \"min\": ").append(pqm.getMinOrder().toPlainString())
+                .append(", \"max\": ").append(pqm.getMaxOrder().toPlainString())
+                .append(", \"step\": ").append(pqm.getStep().toPlainString())
+                .append(", \"value\": ").append(pqm.getMinOrder().toPlainString())
+                .append(", \"title\": \"").append(message.replace("\"", "")).append("\" }");
+
+        addOrReplace(new Label("productAddedObj", outJson.toString()).setEscapeModelStrings(false));
         super.onBeforeRender();
 
         persistCartIfNecessary();
