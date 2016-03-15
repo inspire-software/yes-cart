c37f3f74fb80c9bd70c23ea57bd2796a5fd30691
Igor Azarnyi <Igor_Azarny@epam.com>
2016-03-16 00:15:14 +0100

    [YC-632] Added simple shop vo service

---
+++ manager/jam/src/main/java/org/yes/cart/service/endpoint/ShopEndpointController.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2009- 2016 Denys Pavlov, Igor Azarnyi
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.yes.cart.service.endpoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.yes.cart.domain.vo.VoShop;
+import org.yes.cart.exception.UnableToCreateInstanceException;
+import org.yes.cart.exception.UnmappedInterfaceException;
+import org.yes.cart.service.vo.VoShopService;
+
+import java.util.List;
+
+
+/**
+ * Created by Igor_Azarny on 3/15/2016.
+ */
+@Controller
+@RequestMapping("/shop")
+public class ShopEndpointController {
+
+    private final VoShopService voShopService;
+
+    @Autowired
+    public ShopEndpointController(VoShopService voShopService) {
+        this.voShopService = voShopService;
+    }
+
+    @Secured({"ROLE_SMADMIN","ROLE_SMSHOPADMIN","ROLE_SMWAREHOUSEADMIN","ROLE_SMCALLCENTER","ROLE_SMMARKETINGADMIN"})
+    @RequestMapping(value = "/all", method = RequestMethod.GET,  produces = { MediaType.APPLICATION_JSON_VALUE })
+    public @ResponseBody
+    List<VoShop> getAll() throws UnmappedInterfaceException, UnableToCreateInstanceException {
+        return voShopService.getAll();
+    }
+
+}
