748ad888fe77b8f38f43195a1cf1cd31e867a6b6
denyspavlov <denis.v.pavlov@gmail.com>
2016-08-07 15:43:36 +0100

    [ YC-632 ] YUM webapp
    - refactored the attributes panel into a separate component for reuse (also encapsulation of all the attribute editors)

--- manager/jam-jsclient/src/main/typescript/src/client/app/+shop/components/shop-attributes.component.ts
+++ manager/jam-jsclient/src/main/typescript/src/client/app/+shop/components/shop-attributes.component.ts
@@ -20,6 +20,7 @@ import {PaginationComponent} from './../../shared/pagination/index';
 import {ShopVO, AttrValueShopVO, AttributeVO, Pair} from './../../shared/model/index';
 import {ShopService, ShopEventBus, Util} from './../../shared/services/index';
 import {DataControlComponent} from './../../shared/sidebar/index';
+import {AttributesComponent} from './../../shared/attributes/index';
 import {ModalComponent, ModalResult, ModalAction} from './../../shared/modal/index';
 import {YcValidators} from './../../shared/validation/validators';
 
@@ -27,40 +28,25 @@ import {YcValidators} from './../../shared/validation/validators';
   selector: 'yc-shop-attributes',
   moduleId: module.id,
   templateUrl: 'shop-attributes.component.html',
-  directives: [DataControlComponent, PaginationComponent, REACTIVE_FORM_DIRECTIVES, CORE_DIRECTIVES, ModalComponent]
+  directives: [DataControlComponent, PaginationComponent, REACTIVE_FORM_DIRECTIVES, CORE_DIRECTIVES, ModalComponent, AttributesComponent]
 })
 
 export class ShopAttributesComponent implements OnInit, OnChanges {
 
   @Input() shop:ShopVO;
 
-  //paging
-  maxSize:number = 5;
-  itemsPerPage:number = 10;
-  totalItems:number = 0;
-  currentPage:number = 1;
-  // Must use separate variables (not currentPage) for table since that causes
-  // cyclic even update and then exception https://github.com/angular/angular/issues/6005
-  pageStart:number = 0;
-  pageEnd:number = this.itemsPerPage;
-
-
   shopAttributes:Array<AttrValueShopVO>;
-  shopAttributesRemove:Array<number>;
-  private attributeFilter:string;
-  filteredShopAttributes:Array<AttrValueShopVO>;
+  attributeFilter:string;
 
   changed:boolean = false;
   validForSave:boolean = false;
 
-  @ViewChild('deleteConfirmationModalDialog')
-  deleteConfirmationModalDialog:ModalComponent;
-  @ViewChild('editModalDialog')
-  editModalDialog:ModalComponent;
+  @ViewChild('attributesComponent')
+  attributesComponent:AttributesComponent;
 
   selectedRow:AttrValueShopVO;
 
-  attributeToEdit:AttrValueShopVO;
+  update:Array<Pair<AttrValueShopVO, boolean>>;
 
   /**
    * Construct shop attribute panel
@@ -70,7 +56,7 @@ export class ShopAttributesComponent implements OnInit, OnChanges {
   constructor(private _shopService:ShopService) {
     console.debug('ShopAttributeComponent constructed');
 
-    this.attributeToEdit = null;
+    this.update = [];
 
   }
 
@@ -85,36 +71,20 @@ export class ShopAttributesComponent implements OnInit, OnChanges {
     this.onRefreshHandler();
   }
 
-  /**
-   * Row delete handler.
-   * @param row attribute to delete.
-   */
-  protected onRowDelete(row:AttrValueShopVO) {
-    console.debug('ShopAttributeComponent onRowDelete handler', row);
-    this.deleteConfirmationModalDialog.show();
-  }
-
   protected onRowDeleteSelected() {
     if (this.selectedRow != null) {
-      this.onRowDelete(this.selectedRow);
+      this.attributesComponent.onRowDeleteSelected();
     }
-  }
-
-  protected onRowEdit(row:AttrValueShopVO) {
-    console.debug('ShopAttributeComponent onRowEdit handler', row);
-    this.validForSave = false;
-    this.attributeToEdit = Util.clone(row) ;
-    this.editModalDialog.show();
   }
 
   protected onRowEditSelected() {
     if (this.selectedRow != null) {
-      this.onRowEdit(this.selectedRow);
+      this.attributesComponent.onRowEditSelected();
     }
   }
 
   protected onSelectRow(row:AttrValueShopVO) {
-    console.debug('ShopAttributeComponent onRowPrimary handler', row);
+    console.debug('ShopAttributeComponent onSelectRow handler', row);
     if (row == this.selectedRow) {
       this.selectedRow = null;
     } else {
@@ -122,36 +92,25 @@ export class ShopAttributesComponent implements OnInit, OnChanges {
     }
   }
 
-  onFilterChange(event:any) {
-    this.filterAttributes();
-  }
-
   onDataChange(event:any) {
-    this.validForSave = true;
+    this.validForSave = event.valid;
+    this.update = event.source;
+    this.changed = true;
     console.debug('ShopAttributeComponent data changed and ' + (this.validForSave ? 'is valid' : 'is NOT valid'), event);
   }
 
   protected onSaveHandler() {
     console.debug('ShopAttributeComponent Save handler', this.shop);
-    if (this.shop.shopId > 0 && this.shopAttributes) {
+    if (this.shop.shopId > 0 && this.update) {
 
-      let _update = <Array<Pair<AttrValueShopVO, boolean>>>[];
-      this.shopAttributes.forEach(attr => {
-        if (attr.attrvalueId !== 0 || (attr.val !== null && /\S+.*\S+/.test(attr.val))) {
-          _update.push(new Pair(attr, this.isRemovedAttribute(attr)));
-        }
-      });
+      console.debug('ShopAttributeComponent Save handler update', this.update);
 
-      console.debug('ShopAttributeComponent Save handler update', _update);
-
-      var _sub:any = this._shopService.saveShopAttributes(_update).subscribe(
+      var _sub:any = this._shopService.saveShopAttributes(this.update).subscribe(
           rez => {
+            console.debug('ShopAttributeComponent attributes', rez);
             this.shopAttributes = rez;
-            this.shopAttributesRemove = [];
             this.changed = false;
             this.selectedRow = null;
-            this.filterAttributes();
-            console.debug('ShopAttributeComponent totalItems:' + this.totalItems + ', itemsPerPage:' + this.itemsPerPage);
             _sub.unsubscribe();
         }
       );
@@ -168,44 +127,6 @@ export class ShopAttributesComponent implements OnInit, OnChanges {
     this.getShopAttributes();
   }
 
-  protected onDeleteConfirmationResult(modalresult: ModalResult) {
-    console.debug('ShopAttributeComponent onDeleteConfirmationResult modal result is ', modalresult);
-    if (ModalAction.POSITIVE === modalresult.action) {
-      let attrToDelete = this.selectedRow.attrvalueId;
-      if (attrToDelete === 0) {
-        let idx = this.shopAttributes.findIndex(attrVo => {
-          return attrVo.attribute.code === this.selectedRow.attribute.code;
-        });
-        this.shopAttributes[idx].val = null;
-        console.debug('ShopAttributeComponent onDeleteConfirmationResult index in array of new attribute ' + idx);
-      } else {
-        console.debug('ShopAttributeComponent onDeleteConfirmationResult attribute ' + attrToDelete);
-        this.shopAttributesRemove.push(attrToDelete);
-      }
-      this.filterAttributes();
-      this.selectedRow = null;
-      this.changed = true;
-    }
-  }
-
-  protected onEditModalResult(modalresult: ModalResult) {
-    console.debug('ShopAttributeComponent onEditModalResult modal result is ', modalresult);
-    if (ModalAction.POSITIVE === modalresult.action) {
-      if (this.attributeToEdit.attrvalueId === 0) { // add new
-        console.debug('ShopAttributeComponent onEditModalResult add new attribute', this.shopAttributes);
-        let idx = this.shopAttributes.findIndex(attrVo =>  {return attrVo.attribute.code === this.attributeToEdit.attribute.code;} );
-        this.shopAttributes[idx] = this.attributeToEdit;
-      } else { // edit existing
-        console.debug('ShopAttributeComponent onEditModalResult update existing', this.shopAttributes);
-        let idx = this.shopAttributes.findIndex(attrVo =>  {return attrVo.attrvalueId === this.attributeToEdit.attrvalueId;} );
-        this.shopAttributes[idx] = this.attributeToEdit;
-      }
-      this.selectedRow = this.attributeToEdit;
-      this.changed = true;
-      this.filterAttributes();
-    }
-  }
-
   /**
    * Read attributes, that belong to shop.
    */
@@ -215,92 +136,16 @@ export class ShopAttributesComponent implements OnInit, OnChanges {
 
       this._shopService.getShopAttributes(this.shop.shopId).subscribe(shopAttributes => {
 
-        console.debug('ShopAttributeComponent attributes', this.shopAttributes);
+        console.debug('ShopAttributeComponent attributes', shopAttributes);
         this.shopAttributes = shopAttributes;
-        this.shopAttributesRemove = [];
         this.changed = false;
         this.selectedRow = null;
-
-        this.filterAttributes();
-        console.debug('ShopAttributeComponent totalItems:' + this.totalItems + ', itemsPerPage:' + this.itemsPerPage);
 
       });
     } else {
       this.shopAttributes = null;
-      this.shopAttributesRemove = null;
-      this.filteredShopAttributes = [];
       this.selectedRow = null;
     }
   }
 
-  private filterAttributes() {
-    let _filter = this.attributeFilter ? this.attributeFilter.toLowerCase() : null;
-    if (_filter) {
-      this.filteredShopAttributes = this.shopAttributes.filter(val =>
-        val.attribute.code.toLowerCase().indexOf(_filter) !== -1 ||
-        val.attribute.name.toLowerCase().indexOf(_filter) !== -1 ||
-        val.attribute.description && val.attribute.description.toLowerCase().indexOf(_filter) !== -1 ||
-        val.val && val.val.toLowerCase().indexOf(_filter) !== -1
-      );
-      console.debug('ShopAttributeComponent filterAttributes', _filter);
-    } else {
-      this.filteredShopAttributes = this.shopAttributes;
-      console.debug('ShopAttributeComponent filterAttributes no filter');
-    }
-
-    if (this.filteredShopAttributes === null) {
-      this.filteredShopAttributes = [];
-    }
-
-    console.debug('ShopAttributeComponent filterAttributes', this.filteredShopAttributes);
-
-    let _total = this.filteredShopAttributes.length;
-    this.totalItems = _total;
-    if (_total > 0) {
-      this.resetLastPageEnd();
-    }
-  }
-
-  isRemovedAttribute(row:AttrValueShopVO):boolean {
-    return this.shopAttributesRemove.indexOf(row.attrvalueId) !== -1;
-  }
-
-  isNewAttribute(row:AttrValueShopVO):boolean {
-    return row.attrvalueId == 0 && row.val != null;
-  }
-
-  resetLastPageEnd() {
-    let _pageEnd = this.pageStart + this.itemsPerPage;
-    if (_pageEnd > this.totalItems) {
-      this.pageEnd = this.totalItems;
-    } else {
-      this.pageEnd = _pageEnd;
-    }
-  }
-
-  onPageChanged(event:any) {
-    this.pageStart = (event.page - 1) * this.itemsPerPage;
-    let _pageEnd = this.pageStart + this.itemsPerPage;
-    if (_pageEnd > this.totalItems) {
-      this.pageEnd = this.totalItems;
-    } else {
-      this.pageEnd = _pageEnd;
-    }
-  }
-
-  public getDisplayValue(row:AttrValueShopVO):string {
-    if (row.val != null) {
-      if (row.attribute.etypeName === 'Boolean') {
-        if (row.val === 'true') {
-          return '<i class="fa fa-check-circle"></i>';
-        } else {
-          return '<i class="fa fa-times-circle"></i>';
-        }
-      } else if (row.attribute.etypeName === 'HTML') {
-        return '<pre>' + row.val + '</pre>';
-      }
-      return row.val;
-    }
-    return '&nbsp;';
-  }
 }
---
+++ manager/jam-jsclient/src/main/typescript/src/client/app/shared/attributes/attributes.component.ts
@@ -0,0 +1,296 @@
+/*
+ * Copyright 2009 - 2016 Denys Pavlov, Igor Azarnyi
+ *
+ *    Licensed under the Apache License, Version 2.0 (the 'License');
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an 'AS IS' BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+import {Component, OnInit, OnDestroy, OnChanges, Input, Output, ViewChild, EventEmitter} from '@angular/core';
+import {NgIf, NgFor, CORE_DIRECTIVES } from '@angular/common';
+import {REACTIVE_FORM_DIRECTIVES} from '@angular/forms';
+import {PaginationComponent} from './../pagination/index';
+import {AttrValueVO, AttributeVO, Pair} from './../model/index';
+import {ShopService, ShopEventBus, Util} from './../services/index';
+import {DataControlComponent} from './../sidebar/index';
+import {ModalComponent, ModalResult, ModalAction} from './../modal/index';
+import {YcValidators} from './../validation/validators';
+import {FormValidationEvent} from './../event/index';
+
+@Component({
+  selector: 'yc-attributes',
+  moduleId: module.id,
+  templateUrl: 'attributes.component.html',
+  directives: [DataControlComponent, PaginationComponent, REACTIVE_FORM_DIRECTIVES, CORE_DIRECTIVES, ModalComponent]
+})
+
+export class AttributesComponent implements OnInit, OnChanges {
+
+  @Input() masterObject:any;
+
+  //paging
+  maxSize:number = 5;
+  itemsPerPage:number = 10;
+  totalItems:number = 0;
+  currentPage:number = 1;
+  // Must use separate variables (not currentPage) for table since that causes
+  // cyclic even update and then exception https://github.com/angular/angular/issues/6005
+  pageStart:number = 0;
+  pageEnd:number = this.itemsPerPage;
+
+
+  _objectAttributes:Array<AttrValueVO>;
+  objectAttributesRemove:Array<number>;
+  _attributeFilter:string;
+  filteredObjectAttributes:Array<AttrValueVO>;
+
+  changed:boolean = false;
+  validForSave:boolean = false;
+
+  @ViewChild('deleteConfirmationModalDialog')
+  deleteConfirmationModalDialog:ModalComponent;
+  @ViewChild('editModalDialog')
+  editModalDialog:ModalComponent;
+
+  selectedRow:AttrValueVO;
+
+  attributeToEdit:AttrValueVO;
+
+  @Output() dataSelected: EventEmitter<AttrValueVO> = new EventEmitter<AttrValueVO>();
+  @Output() dataChanged: EventEmitter<FormValidationEvent<Array<Pair<AttrValueVO, boolean>>>> = new EventEmitter<FormValidationEvent<Array<Pair<AttrValueVO, boolean>>>>();
+
+  /**
+   * Construct attribute panel
+   */
+  constructor() {
+    console.debug('AttributesComponent constructed');
+
+    this.attributeToEdit = null;
+
+  }
+
+  @Input()
+  set objectAttributes(objectAttributes:Array<AttrValueVO>) {
+    this._objectAttributes = objectAttributes;
+    this.loadData();
+  }
+
+  get objectAttributes():Array<AttrValueVO> {
+    return this._objectAttributes;
+  }
+
+  @Input()
+  set attributeFilter(attributeFilter:string) {
+    this._attributeFilter = attributeFilter;
+    this.filterAttributes();
+  }
+
+  /** {@inheritDoc} */
+  public ngOnInit() {
+    console.debug('AttributesComponent ngOnInit shop', this.masterObject);
+  }
+
+  ngOnChanges(changes:any) {
+    console.log('AttributesComponent ngOnChanges', changes);
+    this.filterAttributes();
+  }
+
+  private loadData() {
+    if (this.masterObject && this._objectAttributes) {
+
+      console.debug('AttributesComponent attributes', this._objectAttributes);
+      this.objectAttributesRemove = [];
+      this.filterAttributes();
+
+    } else {
+
+      this.objectAttributesRemove = null;
+      this.filteredObjectAttributes = [];
+
+    }
+
+    this.changed = false;
+    this.onSelectRow(null);
+  }
+
+  /**
+   * Row delete handler.
+   * @param row attribute to delete.
+   */
+  protected onRowDelete(row:AttrValueVO) {
+    console.debug('AttributesComponent onRowDelete handler', row);
+    this.deleteConfirmationModalDialog.show();
+  }
+
+  public onRowDeleteSelected() {
+    if (this.selectedRow != null) {
+      this.onRowDelete(this.selectedRow);
+    }
+  }
+
+  protected onRowEdit(row:AttrValueVO) {
+    console.debug('AttributesComponent onRowEdit handler', row);
+    this.validForSave = false;
+    this.attributeToEdit = Util.clone(row) ;
+    this.editModalDialog.show();
+  }
+
+  public onRowEditSelected() {
+    if (this.selectedRow != null) {
+      this.onRowEdit(this.selectedRow);
+    }
+  }
+
+  protected onSelectRow(row:AttrValueVO) {
+    console.debug('AttributesComponent onSelectRow handler', row);
+    if (row == this.selectedRow) {
+      this.selectedRow = null;
+    } else {
+      this.selectedRow = row;
+    }
+    this.dataSelected.emit(this.selectedRow);
+  }
+
+  onDataChange(event:any) {
+    this.validForSave = true;
+    console.debug('AttributesComponent data changed and ' + (this.validForSave ? 'is valid' : 'is NOT valid'), event);
+  }
+
+  private processDataChangesEvent() {
+
+    console.debug('AttributesComponent data changes', this.masterObject);
+    if (this.masterObject && this._objectAttributes) {
+
+      let _update = <Array<Pair<AttrValueVO, boolean>>>[];
+      this._objectAttributes.forEach(attr => {
+        if (attr.attrvalueId !== 0 || (attr.val !== null && /\S+.*\S+/.test(attr.val))) {
+          _update.push(new Pair(attr, this.isRemovedAttribute(attr)));
+        }
+      });
+
+      console.debug('AttributesComponent data changes update', _update);
+
+      this.dataChanged.emit({ source: _update, valid: this.validForSave });
+
+    }
+
+  }
+
+  protected onDeleteConfirmationResult(modalresult: ModalResult) {
+    console.debug('AttributesComponent onDeleteConfirmationResult modal result is ', modalresult);
+    if (ModalAction.POSITIVE === modalresult.action) {
+      let attrToDelete = this.selectedRow.attrvalueId;
+      if (attrToDelete === 0) {
+        let idx = this._objectAttributes.findIndex(attrVo => {
+          return attrVo.attribute.code === this.selectedRow.attribute.code;
+        });
+        this._objectAttributes[idx].val = null;
+        console.debug('AttributesComponent onDeleteConfirmationResult index in array of new attribute ' + idx);
+      } else {
+        console.debug('AttributesComponent onDeleteConfirmationResult attribute ' + attrToDelete);
+        this.objectAttributesRemove.push(attrToDelete);
+      }
+      this.filterAttributes();
+      this.onSelectRow(null);
+      this.changed = true;
+      this.processDataChangesEvent();
+    }
+  }
+
+  protected onEditModalResult(modalresult: ModalResult) {
+    console.debug('AttributesComponent onEditModalResult modal result is ', modalresult);
+    if (ModalAction.POSITIVE === modalresult.action) {
+      if (this.attributeToEdit.attrvalueId === 0) { // add new
+        console.debug('AttributesComponent onEditModalResult add new attribute', this._objectAttributes);
+        let idx = this._objectAttributes.findIndex(attrVo =>  {return attrVo.attribute.code === this.attributeToEdit.attribute.code;} );
+        this._objectAttributes[idx] = this.attributeToEdit;
+      } else { // edit existing
+        console.debug('AttributesComponent onEditModalResult update existing', this._objectAttributes);
+        let idx = this._objectAttributes.findIndex(attrVo =>  {return attrVo.attrvalueId === this.attributeToEdit.attrvalueId;} );
+        this._objectAttributes[idx] = this.attributeToEdit;
+      }
+      this.selectedRow = this.attributeToEdit;
+      this.changed = true;
+      this.filterAttributes();
+      this.processDataChangesEvent();
+    }
+  }
+
+  private filterAttributes() {
+    let _filter = this._attributeFilter ? this._attributeFilter.toLowerCase() : null;
+    if (_filter) {
+      this.filteredObjectAttributes = this._objectAttributes.filter(val =>
+        val.attribute.code.toLowerCase().indexOf(_filter) !== -1 ||
+        val.attribute.name.toLowerCase().indexOf(_filter) !== -1 ||
+        val.attribute.description && val.attribute.description.toLowerCase().indexOf(_filter) !== -1 ||
+        val.val && val.val.toLowerCase().indexOf(_filter) !== -1
+      );
+      console.debug('AttributesComponent filterAttributes', _filter);
+    } else {
+      this.filteredObjectAttributes = this._objectAttributes;
+      console.debug('AttributesComponent filterAttributes no filter');
+    }
+
+    if (this.filteredObjectAttributes === null) {
+      this.filteredObjectAttributes = [];
+    }
+
+    console.debug('AttributesComponent filterAttributes', this.filteredObjectAttributes);
+
+    let _total = this.filteredObjectAttributes.length;
+    this.totalItems = _total;
+    if (_total > 0) {
+      this.resetLastPageEnd();
+    }
+  }
+
+  isRemovedAttribute(row:AttrValueVO):boolean {
+    return this.objectAttributesRemove.indexOf(row.attrvalueId) !== -1;
+  }
+
+  isNewAttribute(row:AttrValueVO):boolean {
+    return row.attrvalueId == 0 && row.val != null;
+  }
+
+  resetLastPageEnd() {
+    let _pageEnd = this.pageStart + this.itemsPerPage;
+    if (_pageEnd > this.totalItems) {
+      this.pageEnd = this.totalItems;
+    } else {
+      this.pageEnd = _pageEnd;
+    }
+  }
+
+  onPageChanged(event:any) {
+    this.pageStart = (event.page - 1) * this.itemsPerPage;
+    let _pageEnd = this.pageStart + this.itemsPerPage;
+    if (_pageEnd > this.totalItems) {
+      this.pageEnd = this.totalItems;
+    } else {
+      this.pageEnd = _pageEnd;
+    }
+  }
+
+  public getDisplayValue(row:AttrValueVO):string {
+    if (row.val != null) {
+      if (row.attribute.etypeName === 'Boolean') {
+        if (row.val === 'true') {
+          return '<i class="fa fa-check-circle"></i>';
+        } else {
+          return '<i class="fa fa-times-circle"></i>';
+        }
+      } else if (row.attribute.etypeName === 'HTML') {
+        return '<pre>' + row.val + '</pre>';
+      }
+      return row.val;
+    }
+    return '&nbsp;';
+  }
+}
