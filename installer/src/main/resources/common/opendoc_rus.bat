127550c908f0ee2e9bdbbe445c35d291f5b0eea2
denyspavlov <denis.v.pavlov@gmail.com>
2018-01-16 20:47:34 +0000

    [ YC-850 ] Remove installer module from codebase
    As we now entered the time of clouds and virtual machines there is no longer a need for installer.
    As of 3.4.0 this module will be removed from code base.
    We have AWS install guide for someone who want own installation without going through the setup from source guide: http://www.inspire-software.com/confluence/display/YC3J/Running+Yes-Cart+on+AWS

--- installer/src/main/resources/common/opendoc_rus.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rem
-rem YesCart. Open Yes-Cart documentation
-rem @author Igor Azarny (iazarny@yahoo.com)
-
-start http://www.inspire-software.com/confluence/display/YC3RU/YC+3.x.x+Wiki
-
-exit
