133409b178fc79ec7eb0a4a0bc6d8300df07fbf2
denyspavlov <denis.v.pavlov@gmail.com>
2018-06-09 19:24:07 +0100

    [ YC-000 ] org.yes.cart.util.DateUtilsTest localization fail #44
    - changed UK locale to US in tests in anticipation that it would be more widely available

--- domain-api/src/test/java/org/yes/cart/util/DateUtilsTest.java
+++ domain-api/src/test/java/org/yes/cart/util/DateUtilsTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2009 Denys Pavlov, Igor Azarnyi
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package org.yes.cart.util;
 
 import org.junit.Test;
@@ -199,10 +215,10 @@ public class DateUtilsTest {
     @Test
     public void formatCustomer() throws Exception {
 
-        assertEquals("10 October 2017", DateUtils.formatCustomer(DateUtils.iParseSDT("2017-10-10 10:30:00"), Locale.UK));
-        assertEquals("10 October 2017", DateUtils.formatCustomer(DateUtils.ldParseSDT("2017-10-10 10:30:00"), Locale.UK));
-        assertEquals("10 October 2017", DateUtils.formatCustomer(DateUtils.ldtParseSDT("2017-10-10 10:30:00"), Locale.UK));
-        assertEquals("10 October 2017", DateUtils.formatCustomer(DateUtils.zdtParseSDT("2017-10-10 10:30:00"), Locale.UK));
+        assertEquals("10 October 2017", DateUtils.formatCustomer(DateUtils.iParseSDT("2017-10-10 10:30:00"), Locale.US));
+        assertEquals("10 October 2017", DateUtils.formatCustomer(DateUtils.ldParseSDT("2017-10-10 10:30:00"), Locale.US));
+        assertEquals("10 October 2017", DateUtils.formatCustomer(DateUtils.ldtParseSDT("2017-10-10 10:30:00"), Locale.US));
+        assertEquals("10 October 2017", DateUtils.formatCustomer(DateUtils.zdtParseSDT("2017-10-10 10:30:00"), Locale.US));
 
     }
 
